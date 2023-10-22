06c86cf34bc9e7eaaa5b29024931cfb3f54c3a9d
tekitouk <47982907+koduma@users.noreply.github.com>
2023-10-22 16:34:06 +0900

    Update gene.cpp

--- gene.cpp
+++ gene.cpp
@@ -1,53 +1,47 @@
 //dp
 #include <stdio.h>
-#define E (1e-4)
+static int D0[51*2][51*2];
+static int D[51*2][51*2];
 int main(){
-    int a, c;
-    scanf("%d %d", &a, &c);
-    double t = c / 60.0;
-    t *= 10;
-    t += 0.5;
-    int q = (int)t;
-    double b = q / 10.0;
-    if (b <= 0.2 + E) {
-        printf("C 0\n");
-        return 0;
-    }
-    if (a > 1687 && a <= 1912) printf("S");
-    else if (a > 112 && a <= 337) printf("NNE");
-    else if (a > 1912 && a <= 2137) printf("SSW");
-    else if (a > 337 && a <= 562) printf("NE");
-    else if (a > 2137 && a <= 2362) printf("SW");
-    else if (a > 562 && a <= 787) printf("ENE");
-    else if (a > 2362 && a <= 2587) printf("WSW");
-    else if (a > 787 && a <= 1012) printf("E");
-    else if (a > 2587 && a <= 2812) printf("W");
-    else if (a > 1012 && a <= 1237) printf("ESE");
-    else if (a > 2812 && a <= 3037) printf("WNW");
-    else if (a > 1237 && a <= 1462) printf("SE");
-    else if (a > 3037 && a <= 3262) printf("NW");
-    else if (a > 1462 && a <= 1687) printf("SSE");
-    else if (a > 3262 && a <= 3487) printf("NNW");
-    else printf("N");
-    printf(" ");
-    if (b >= 8 - E && b <= 10.7 + E) printf("5");
-    else if (b >= 24.5 - E && b <= 28.4 + E) printf("10");
-    else if (b >= 0.3 - E && b <= 1.5 + E) printf("1");
-    else if (b >= 10.8 - E && b <= 13.8 + E) printf("6");
-    else if (b >= 28.5 - E && b <= 32.6 + E) printf("11");
-    else if (b >= 1.6 - E && b <= 3.3 + E) printf("2");
-    else if (b >= 13.9 - E && b <= 17.1 + E) printf("7");
-    else if (b >= 3.4 - E && b <= 5.4 + E) printf("3");
-    else if (b >= 17.2 - E && b <= 20.7 + E) printf("8");
-    else if (b >= 5.5 - E && b <= 7.9 + E) printf("4");
-    else if (b >= 20.8 - E && b <= 24.4 + E) printf("9");
-    else printf("12");
-    printf("\n");
-    return 0;
-}
-int max(int a, int b) {
-  if (a >= b)
-    return a;
-  else
-    return b;
+	int N;
+	int Q;
+	int i, j, k;
+	scanf("%d", &N);
+	for (i = 1; i <= N; ++i) {
+		for (j = 1; j <= N; ++j) {
+			int d;
+			scanf("%d", &d);
+			D0[i][j] = d;
+		}
+	}
+	for (i = 1; i <= N * 2; ++i) {
+		for (j = 1; j <= N * 2; ++j) {
+			D[i][j] = D0[i][j] + D[i - 1][j] + D[i][j - 1] - D[i - 1][j - 1];
+		}
+	}
+	scanf("%d", &Q);
+	for (k = 0; k < Q; ++k) {
+		int p;
+		int s;
+		int taste = 0;
+		scanf("%d", &p);
+		for (s = 1; s * s <= p; ++s) {
+			int t = p / s;
+			if (t > N) t = N;
+			for (i = 0; i < N; ++i) {
+				for (j = 0; j < N; ++j) {
+					int taste1 = D[i + s][j + t] - D[i][j + t] - D[i + s][j] + D[i][j];
+					int taste2 = D[i + t][j + s] - D[i][j + s] - D[i + t][j] + D[i][j];
+					if (taste < taste1) {
+						taste = taste1;
+					}
+					if (taste < taste2) {
+						taste = taste2;
+					}
+				}
+			}
+		}
+		printf("%d\n", taste);
+	}
+	return 0;
 }
