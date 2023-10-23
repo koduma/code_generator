b0a37f26c277b4478b8d89f059dc720f08267a26
tekitouk <47982907+koduma@users.noreply.github.com>
2023-10-23 19:17:34 +0900

    Delete compile.py

--- compile.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-import os
-import time
-r=1-subprocess.run([r"/usr/bin/g++", "-Wall", "-o", "gene", 'gene.cpp'], stdout=subprocess.PIPE, stderr=subprocess.PIPE).returncode
-f = open('log.txt', 'w')#1は成功、0は失敗
-f.write(str(r))
-f.close()
